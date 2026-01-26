50de4cdfb9893fb3114e4cc5f33beb244c18085e
ccanning-stack <147844781+ccanning-stack@users.noreply.github.com>
2026-01-26 20:19:05 +0000

    fix makefile and gitignore for common include library, update hex2int

--- src/hex2int.c
+++ src/hex2int.c
@@ -1,11 +1,23 @@
 #include "common_hdrs.h"
 #include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+int calculateTotal(char *argv) {
+
+  int total = 0;
+  for (int i = 0; argv[i] != '\0'; i++) {
+    total += (unsigned char)argv[i];
+  }
+  return total;
+}
 
 bool isValidHex(char *arg) {
 
   size_t argLength = strlen(arg);
 
-  for (int i = 0; i <= argLength; i++) {
+  for (int i = 0; i < argLength; i++) {
 
     if (isxdigit((unsigned char)arg[i]) == 0) {
       return false;
@@ -13,36 +25,28 @@ bool isValidHex(char *arg) {
       return true;
     }
   }
+}
 
-  void covertHexAndPrintInt(char *argv) {
+void convertHexAndPrintInt(char *argv) {
 
-    if (isValidHex(argv)) {
-      int total = calculateTotal(argv);
-      printf("%s = %d", argv, total);
-    } else {
-      printf("%s: not a valid number", argv);
-    }
+  if (isValidHex(argv)) {
+    int total = calculateTotal(argv);
+    printf("%s = %d", argv, total);
+  } else {
+    printf("%s: not a valid number", argv);
+  }
+}
+
+int main(int argc, char *argv[]) {
+
+  int index = 1;
+
+  if (argc < 2 || argc > 5) {
+
+    printf("Usage: Must be between 1 and 4 arguments");
   }
 
-  int calculateTotal(char *argv) {
-
-    int total = 0;
-    for (int i = 0; argv[i] != "\0"; i++) {
-      total += (unsigned char)argv[i];
-    }
-    return total;
+  while ((argv[index])++ != NULL) {
+    convertHexAndPrintInt(argv[index]);
   }
-
-  int main(int argc, char *argv[]) {
-
-    int index = 1;
-
-    if (argc < 2 || argc > 5) {
-
-      printf("Usage: Must be between 1 and 4 arguments");
-    }
-
-    while ((argv[index])++ != "\0") {
-      convertHexAndPrintInt(argv[index]);
-    }
-  }
+}
