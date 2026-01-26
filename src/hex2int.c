bdbbb375879828f86c32a910cbe9c36c5bcaf63c
ccanning-stack <147844781+ccanning-stack@users.noreply.github.com>
2026-01-26 22:02:29 +0000

    update Makefile for generic builds and finalish hex2int

--- src/hex2int.c
+++ src/hex2int.c
@@ -2,6 +2,7 @@
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int calculateTotal(char *argv) {
@@ -9,8 +10,19 @@ int calculateTotal(char *argv) {
   int total = 0;
 
   for (int i = 2; argv[i] != '\0'; i++) {
-    total += (unsigned char)argv[i];
+    unsigned char c = (unsigned char)argv[i];
+    int v;
+
+    if (c >= '0' && c <= '9')
+      v = c - '0';
+    else if (c >= 'a' && c <= 'f')
+      v = c - 'a' + 10;
+    else
+      v = c - 'A' + 10;
+
+    total = total * 16 + v;
   }
+
   return total;
 }
 
@@ -33,11 +45,15 @@ bool isValidHex(char *arg) {
 
 void convertHexAndPrintInt(char *argv) {
 
+  if (argv == NULL) {
+    return;
+  }
+
   if (isValidHex(argv)) {
     int total = calculateTotal(argv);
-    printf("%s = %d", argv, total);
+    printf("%s = %d\n", argv, total);
   } else {
-    printf("%s: not a valid number", argv);
+    printf("%s: not a valid number\n", argv);
   }
 }
 
@@ -48,9 +64,13 @@ int main(int argc, char *argv[]) {
   if (argc < 2 || argc > 5) {
 
     printf("Usage: Must be between 1 and 4 arguments");
+    return EXIT_FAILURE;
   }
 
-  while ((argv[index])++ != NULL) {
+  while (argv[index] != NULL) {
     convertHexAndPrintInt(argv[index]);
+    index++;
   }
+
+  return EXIT_SUCCESS;
 }
