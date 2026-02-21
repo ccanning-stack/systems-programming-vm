12dca46a254464cda410f358832cbb618bdbaed3
ccanning-stack <147844781+ccanning-stack@users.noreply.github.com>
2026-02-21 17:30:52 +0000

    add dynamic array and comparator

--- src/sortprint.c
+++ src/sortprint.c
@@ -5,33 +5,49 @@
 #include <string.h>
 
 typedef struct {
-  char *items;
+  const char **items;
   size_t count;
   size_t capacity;
-} Dynamicarray;
+} Words;
+
+#define da_append(da, elem)                                                    \
+  do {                                                                         \
+    if (da.count >= da.capacity) {                                             \
+      if (da.capacity == 0) {                                                  \
+        da.capacity = 256;                                                     \
+      } else {                                                                 \
+        da.capacity *= 2;                                                      \
+      }                                                                        \
+      da.items = realloc(da.items, da.capacity * sizeof(*da.items));           \
+    }                                                                          \
+    da.items[da.count++] = elem;                                               \
+  } while (0)
+
+int cmp_strcoll(const void *a, const void *b) {
+  const char *const *sa = a;
+  const char *const *sb = b;
+
+  return strcoll(*sa, *sb);
+}
 
 int main(int argc, char *argv[]) {
   setlocale(LC_ALL, "");
 
-  printf("%s\n", setlocale(LC_COLLATE, NULL));
-
-  // Dynamicarray sortedwords = {0};
+  Words words = {0};
 
   if (argc < 2 || argc > 7) {
     printf("Usage: Must be between 1 and 6 arguments");
     return EXIT_FAILURE;
   }
 
-  char *s1 = argv[1];
-  char *s2 = argv[2];
-
-  int numcomp = strcoll(s1, s2);
-
-  if (numcomp < 0) {
-    printf("First word: %s\n", s1);
-  } else {
-    printf("First word: %s\n", s2);
+  for (int i = 1; i <= argc; i++) {
+    da_append(words, argv[i]);
   }
 
+  qsort(words.items, words.count, sizeof(*words.items), cmp_strcoll);
+
+  while (words.count > 0) {
+    printf("%s\n", words.items[words.count]);
+  }
   return EXIT_SUCCESS;
 }
