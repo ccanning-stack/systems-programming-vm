7d456b10c8793c3e2177367e932d3be9f2b77904
ccanning-stack <147844781+ccanning-stack@users.noreply.github.com>
2026-05-26 20:32:49 +0100

    add .txt to gitignore, finish spl_cp1.c, add debug to Makefile

--- src/spl_cp1.c
+++ src/spl_cp1.c
@@ -2,90 +2,109 @@
 #include "../common/common_hdrs.h"
 #include <stdlib.h>
 #include <unistd.h>
-#define MESSAGE_SIZE 512
-#define PERMISSIONS                                                            \
-  S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH /* rw-rw-r-- */
-int main(int argc, char *argv[]) {
-  int source_fd;                    /* Source file descriptor */
-  int target_fd;                    /* Target file descriptor */
-  int num_bytes_read;               /* Return value of read() */
-  int num_bytes_written;            /* Return value of write() */
-  mode_t permissions = PERMISSIONS; /* Permissions to assign */
-  char message[MESSAGE_SIZE];       /* Error message string */
-  int default_buffer = 4096;
-  char *user_defined_buffer;
-  int BUFFER_SIZE = default_buffer;
-  int ch;
+#include <errno.h>
+#include <fcntl.h>
+#include <libgen.h>
+#include <sys/stat.h>
 
-  char buffer[BUFFER_SIZE];
+#define DEFAULT_BUFFER_SIZE 4096
 
-  while ((ch = getopt(argc, argv, "B:")) != -1) {
+#define PERMISSIONS \
+    (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 
-    switch (ch) {
-    case 'B':
-      printf("OPTARG: %d", (int)strtol(optarg, NULL, 10));
-      break;
-    case ':':
-      printf("required option not found");
-      break;
-    case '?':
-      printf("required option not found");
-      break;
+int main(int argc, char *argv[])
+{
+    int source_fd;
+    int target_fd;
+    ssize_t num_bytes_read;
+    ssize_t num_bytes_written;
+
+    mode_t permissions = PERMISSIONS;
+    char message[512];
+
+    int buffer_size = DEFAULT_BUFFER_SIZE;
+    void *buffer = NULL;
+
+    int opt;
+
+    while ((opt = getopt(argc, argv, ":B:")) != -1) {
+        switch (opt) {
+        case 'B': {
+            char *end;
+            long val = strtol(optarg, &end, 10);
+
+            if (*end != '\0' || val <= 0) {
+                usage_error("invalid buffer size");
+            }
+
+            buffer_size = (int)val;
+            break;
+        }
+
+        case ':':
+            usage_error("missing argument for -B");
+
+        case '?':
+            usage_error("invalid option");
+        }
     }
 
-    /* Check for correct usage. */
-    if (argc != 3 && argc != 4) {
-      sprintf(message, "%s source destination", basename(argv[0]));
-      usage_error(message);
+    if (argc - optind != 2) {
+        sprintf(message, "%s source destination", basename(argv[0]));
+        usage_error(message);
     }
 
-    /* Open source file for reading. */
-    errno = 0;
-    if ((source_fd = open(argv[1], O_RDONLY)) == -1) {
-      sprintf(message, "unable to open %s for reading", argv[1]);
-      fatal_error(errno, message);
+    buffer = malloc(buffer_size);
+    if (buffer == NULL) {
+        fatal_error(errno, "malloc failed");
     }
 
-    /* Open target file for writing. */
-    if ((target_fd =
-             open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, permissions)) == -1) {
-      sprintf(message, "unable to open %s for writing", argv[2]);
-      fatal_error(errno, message);
+    source_fd = open(argv[optind], O_RDONLY);
+    if (source_fd == -1) {
+        sprintf(message, "unable to open %s for reading", argv[optind]);
+        fatal_error(errno, message);
     }
 
-    /* Repeatedly transfer BUFFER_SIZE bytes at a time from source_fd to
-    target_fd. */
-    errno = 0;
-    while ((num_bytes_read = read(source_fd, buffer, BUFFER_SIZE)) > 0) {
-      errno = 0;
-      num_bytes_written = write(target_fd, buffer, num_bytes_read);
-      if (errno != 0)
-        fatal_error(errno, "copy");
-      else if (num_bytes_written != num_bytes_read) {
-        sprintf(message, "write error to %s\n", argv[2]);
-        fatal_error(-1, message);
-      }
+    target_fd = open(argv[optind + 1],
+                     O_WRONLY | O_CREAT | O_TRUNC,
+                     permissions);
+
+    if (target_fd == -1) {
+        sprintf(message, "unable to open %s for writing", argv[optind + 1]);
+        fatal_error(errno, message);
     }
 
-    if (num_bytes_read == -1)
-      fatal_error(errno, "error reading");
+    while ((num_bytes_read = read(source_fd, buffer, buffer_size)) > 0) {
+        num_bytes_written = write(target_fd, buffer, num_bytes_read);
 
-    /* Close files. */
+        if (num_bytes_written == -1) {
+            fatal_error(errno, "write error");
+        }
+
+        if (num_bytes_written != num_bytes_read) {
+            fatal_error(-1, "partial write error");
+        }
+    }
+
+    if (num_bytes_read == -1) {
+        fatal_error(errno, "read error");
+    }
+
     if (close(source_fd) == -1) {
-      sprintf(message, "error closing source file %s", argv[1]);
-      fatal_error(errno, message);
+        sprintf(message, "error closing source file %s", argv[optind]);
+        fatal_error(errno, message);
     }
 
-    errno = 0;
+    if (fsync(target_fd) == -1) {
+        fatal_error(errno, "fsync error");
+    }
 
-    if (-1 == fsync(target_fd)) /* Flush data to device. */
-      fatal_error(errno, "fsync");
-
-    /* fsync() was successful. */
     if (close(target_fd) == -1) {
-      sprintf(message, "error closing target file %s", argv[2]);
-      fatal_error(errno, "error closing target file");
+        sprintf(message, "error closing target file %s", argv[optind + 1]);
+        fatal_error(errno, "error closing target file");
     }
+
+    free(buffer);
+
     return 0;
-  }
 }
